f4a7ca10b019e31ff9a373c54f32e029affe4af9
Yuvraj-Malik <malikyuvraj2701@gmail.com>
2025-08-28 10:12:00 +0530

    Assignment-3

---
+++ Assignment-3/evaluate_postfix.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int x)
+{
+    if (top == MAX - 1)
+    {
+        printf("Stack Overflow\n");
+        exit(1);
+    }
+    stack[++top] = x;
+}
+
+int pop()
+{
+    if (top == -1)
+    {
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    return stack[top--];
+}
+
+int evaluatePostfix(char *exp)
+{
+    int i;
+    for (i = 0; exp[i] != '\0'; i++)
+    {
+        if (isdigit(exp[i]))
+        {
+            push(exp[i] - '0');
+        }
+        else
+        {
+            int val2 = pop();
+            int val1 = pop();
+            switch (exp[i])
+            {
+            case '+':
+                push(val1 + val2);
+                break;
+            case '-':
+                push(val1 - val2);
+                break;
+            case '*':
+                push(val1 * val2);
+                break;
+            case '/':
+                push(val1 / val2);
+                break;
+            default:
+                printf("Invalid operator: %c\n", exp[i]);
+                exit(1);
+            }
+        }
+    }
+    return pop();
+}
+
+int main()
+{
+    char exp[MAX];
+    printf("Enter a postfix expression: ");
+    scanf("%s", exp);
+
+    int result = evaluatePostfix(exp);
+    printf("Result of Postfix expression: %d\n", result);
+
+    return 0;
+}
